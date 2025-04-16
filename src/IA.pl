b062e2d1abe130fa14a445edf6ee6a6848e9878e
Nils De Bruycker <115474598+NilsDeBruycker@users.noreply.github.com>
2025-04-16 12:34:36 +0200

    pond 2eme plus algo arbre très abstrait

--- src/IA.pl
+++ src/IA.pl
@@ -26,11 +26,27 @@ retirerpond((Lutins,Ponds),Pond,(Lutins,Ponds_mod)):- \+ member(Ponds,Pond),appe
 %%pour droite et haut = base reste la meme mais destination change
 
 rotate_pond((Lutins,Ponds),((Xp,Yp),(Xp2,Yp2)),((Xp,Yp),(Xp2_mod,Yp2_mod)),Ponds_mod()):-check_pond(((Xp,Yp),(Xp2,Yp2))),%%check que la position de base du pond est valide
-\+ member(Ponds,Pond),%% check que le pond existe
+\+ member(Ponds,((Xp,Yp),(Xp2,Yp2))),%% check que le pond existe
 member(((Xp,Yp),(Xp2_mod,Yp2_mod)),Ponds),%% check que place de rotation est libre
-apend(Ponds,Pond,X),%% X= ponds avec anciene position de pond ajouter
+apend(Ponds,((Xp,Yp),(Xp2,Yp2)),X),%% X= ponds avec anciene position de pond ajouter
 check_pond((Xp,Yp),(Xp2_mod,Yp2_mod)),%% check nouvelle position est valide
 delete(X,((Xp,Yp),(Xp2_mod,Yp2_mod)),Ponds_mod).%% pont mod = X ou retire la nouvelle position du pond 
 
+%% rotate pond
+rotate_pond((Lutins,Ponds),((Xp,Yp),(Xp2,Yp2)),((Xp_mod,Yp_mod),(Xp2,Yp2)),Ponds_mod()):-check_pond(((Xp,Yp),(Xp2,Yp2))),%%check que la position de base du pond est valide
+\+ member(Ponds,((Xp,Yp),(Xp2,Yp2))),%% check que le pond existe
+member(((Xp_mod,Yp_mod),(Xp2,Yp2)),Ponds),%% check que place de rotation est libre
+apend(Ponds,((Xp,Yp),(Xp2,Yp2)),X),%% X= ponds avec anciene position de pond ajouter
+check_pond((Xp_mod,Yp_mod),(Xp2,Yp2)),%% check nouvelle position est valide
+delete(X,((Xp_mod,Yp_mod),(Xp2,Yp2)),Ponds_mod).%% pont mod = X ou retire la nouvelle position du pond 
 %% pour quand tour d'un joueur se fini devra check lutins et si une team est éliminer les suprimer de la liste des lutin 
+
+
 eliminate()
+
+tree_node(value,min_or_max,game_state,depth).
+%%if depht = 0, heuristique (game state,value),
+%% else faire enfants (jouer selon heuristique) et update en direct selon ligne suivante
+%% value = if min_or_max, min(val enfant ,value)) else value =max (valeur enfant,enfant value)
+%% if min and value.parent > value cut car aplpha beta elsif max and parent.value < value cut car alpha beta
+
